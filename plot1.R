d7abaf3a3398821ad205cc267e6de5097a46d54c
Carlo Scarioni <carlo.scarioni@gmail.com>
2015-12-13 17:18:41 +0000

    using png

--- plot1.R
+++ plot1.R
@@ -4,6 +4,6 @@ if(!file.exists("./data/dataset.zip")) {
 fromFile <-  unz("./data/dataset.zip", "household_power_consumption.txt")
 data <- read.table(fromFile, sep = ";", header = TRUE)
 forDate <- data[as.Date(data$Date, "%d/%m/%Y") >= as.Date('2007-02-01') & as.Date(data$Date, "%d/%m/%Y") <= as.Date('2007-02-02'), ]
+png("./plot1.png")
 hist(as.double(as.character(forDate$Global_active_power)), col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
-dev.copy(png, "./plot1.png")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -6,7 +6,7 @@ data <- read.table(fromFile, sep = ";", header = TRUE)
 forDate <- data[as.Date(data$Date, "%d/%m/%Y") >= as.Date('2007-02-01') & as.Date(data$Date, "%d/%m/%Y") <= as.Date('2007-02-02'), ]
 forDate$Timestamp <- NA
 forDate$Timestamp <- paste(forDate$Date, forDate$Time, sep = " ")
+png("./plot2.png")
 plot(strptime(forDate$Timestamp, format = "%d/%m/%Y %H:%M:%S"), as.double(as.character(forDate$Global_active_power)), type = "l", ann=FALSE)
 title(ylab="Global Active Power (kilowatts)")
-dev.copy(png, "./plot2.png")
 dev.off()
